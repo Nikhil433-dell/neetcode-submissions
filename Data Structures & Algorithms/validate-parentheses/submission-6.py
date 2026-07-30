b3ddf8784942cbdbfdf3e2ec2ccbf4056104b081
Nikhil Jangir <82225430+Nikhil433-dell@users.noreply.github.com>
2026-07-30 13:57:16 -0500

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str):
+
+        myDict = {
+            "]": "[",
+            "}": "{",
+            ")": "("
+        }
+
+        myStack = []
+        
+        for i in s:
+            if i in myDict:
+                if myStack and myStack[-1] == myDict[i]:
+                    myStack.pop()
+                else:
+                    return False
+            else:
+                myStack.append(i)
+                
+            
+
+        return True if not myStack else False
+
+
